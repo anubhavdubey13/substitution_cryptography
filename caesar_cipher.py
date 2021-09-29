0469473db1038f476858fae19b593d96944d5b05
Anubhav Dubey <anubhav.dubey.stats@gmail.com>
2021-09-29 11:19:11 +0530

    checked for negative indexing which works by default in Python. Next steps, apply modulo and create final encryption function.

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -10,20 +10,29 @@ original_text = input('Enter the text: ').lower()
 #print(original_text)
 
 # To shift by
-push_by = int(input('Number of spaces to be shifted: '))
+spaces = int(input('Number of spaces to be shifted: '))
+
+# Not needed 
+#if spaces < 0:
+ #   push_by = 26 + spaces
+#else:
+ #   push_by = spaces
+
+push_by = spaces
 
 # Initializing an empty list to store the results from iteration
 output = []
 
-# What is left: 1. how to shift using push_by to create the encrypted code
+# I think division is a much better approach. Need to figure out dealing with reminders
+# Encryption
 for o in original_text:
     if o in list_of_alphabet:
         if list_of_alphabet.index(o) + push_by < 26:
             o = list_of_alphabet[list_of_alphabet.index(o) + push_by]
-            print(o)
+            #print(o)
         else:
             o = list_of_alphabet[list_of_alphabet.index(o) + push_by - 26]
-            print(o)
+            #print(o)
         output.append(o)
     else:
         output.append(o)
@@ -31,8 +40,12 @@ for o in original_text:
 print(''.join(output))
 
 
+#========= Rough Work ===============
 if 'o' in list_of_alphabet:
     print(list_of_alphabet[list_of_alphabet.index('o') + 12 - 26])
     
     
 
+push = -3
+push_you = 26 + push
+print(list_of_alphabet[list_of_alphabet.index('a') + push_you])
